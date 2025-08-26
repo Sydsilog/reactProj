b4af57da54044aae8369de289cda6c1da591b223
Sydsilog <christopher.sy@cvsu.edu.ph>
2025-08-27 02:30:27 +0800

    NavBar

--- src/components/navBar.tsx
+++ src/components/navBar.tsx
@@ -1,13 +1,18 @@
 const navBar = () => {
   return (
-    <div className="bg-black flex h-17">
-      <div id="left" className="w-2/4"></div>
-      <div id="right" className="w-2/4 bg-red-700">
-        <ul className="text-white flex justify-evenly items-center h-full">
-          <li>Home</li>
-          <li>About</li>
-          <li>Contact</li>
-          <li>Log Out</li>
+    <div className="bg-black flex h-17 ">
+      <div id="left" className="w-2/4 flex  items-center">
+        <div className="bg-white w-10 h-10 rounded-full mx-5"></div>
+        <h1 className="text-white">CHRISTOPHER</h1>
+      </div>
+      <div id="right" className="w-2/4 ">
+        <ul className="text-orange-600 flex justify-evenly items-center h-full">
+          <li>HOME</li>
+          <li>ABOUT</li>
+          <li>CONTACT</li>
+          <li className="bg-orange-600 text-black h-9 w-20 rounded-2xl flex justify-center items-center">
+            LOG OUT
+          </li>
         </ul>
       </div>
     </div>
